4d81e44593b9d56a9cf38abbc7fc1a2dc26c8102
Luis Beaucamp <luis.beaucamp@uni-ulm.de>
2020-03-22 15:05:44 +0100

    add Resources folder, add Playing Card Sprite Loader

--- Assets/Scripts/PlayingCard.cs
+++ Assets/Scripts/PlayingCard.cs
@@ -1,18 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayingCard : MonoBehaviour
+public class PlayingCard
 {
-    // Start is called before the first frame update
-    void Start()
+    public Suit CardSuit { get; set; }
+    public Rank CardRank { get; set; }
+    public bool IsTrump { get; set; }
+    public Sprite Image { get; set; }
+
+    public enum Suit
     {
-        
+        Blatt,
+        Eichel,
+        Schelln,
+        Herz
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum Rank
     {
-        
+        Sieben,
+        Acht,
+        Neun,
+        Koenig,
+        Zehn,
+        Unter,
+        Ober,
+        Ass
+    }
+
+    public Sprite GetSprite()
+    {
+        return GetSprite(CardSuit, CardRank);
+    }
+    
+
+    
+    
+    
+    
+    public static Sprite GetSprite(Suit suit, Rank rank)
+    {
+        string pathSuffix = "";
+        pathSuffix += suit.ToString().ToLower();
+        pathSuffix += GetRankFileSuffix(rank);
+        return Resources.Load<Sprite>($"Spielkarten/{pathSuffix}");
+    }
+
+    private static string GetRankFileSuffix(Rank rank)
+    {
+        switch (rank)
+        {
+            case Rank.Sieben:
+                return "07";
+            case Rank.Acht:
+                return "08";
+            case Rank.Neun:
+                return "09";
+            case Rank.Koenig:
+                return "ko";
+            case Rank.Zehn:
+                return "10";
+            case Rank.Unter:
+                return "un";
+            case Rank.Ober:
+                return "ob";
+            case Rank.Ass:
+                return "as";
+            default:
+                throw new ArgumentOutOfRangeException(nameof(rank), rank, null);
+        }
     }
 }
