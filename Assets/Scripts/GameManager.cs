a43ee5b032f6e3acce4af37d75eedb10ee7bb656
lursyy <13033130+lursyy@users.noreply.github.com>
2022-12-20 20:30:31 +0100

    add Resources folder, add Playing Card Sprite Loader

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Button button;
+    [SerializeField] private PlayingCard.Suit testSuit;
+    [SerializeField] private PlayingCard.Rank testRank;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        button.image.sprite = PlayingCard.GetSprite(testSuit, testRank);
     }
 
     // Update is called once per frame
