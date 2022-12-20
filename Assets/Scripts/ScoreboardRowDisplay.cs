e177eba38abebdafd7d55ead65669138b1a4d659
lursyy <13033130+lursyy@users.noreply.github.com>
2022-12-20 20:30:36 +0100

    add scoreboard UI

---
+++ Assets/Scripts/ScoreboardRowDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class ScoreboardRowDisplay : MonoBehaviour
+{
+    public List<Text> scoreTexts;
+
+    public void FillScoreTexts(Extensions.ScoreBoardRow row)
+    {
+        Assert.AreEqual(scoreTexts.Count, row.EntryCount);
+
+        for (var i = 0; i < scoreTexts.Count; i++)
+        {
+            scoreTexts[i].text = row.Entries[i].score.ToString();
+        }
+    }
+}
